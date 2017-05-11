81814298857bf7b5e88885a3150c814821afc810
Jufebrown <jufebrown@gmail.com>
2017-05-11 16:00:24 -0500

    declared base url at top of function instead of inside each of methods

--- app/factories/authFactory.js
+++ app/factories/authFactory.js
@@ -1,8 +1,10 @@
 app.factory('authFactory', function($http) {
+
+  const baseURL = 'http://localhost:3000/api/v1/auth/';
+
   return {
 
     register: function({username, password}) {
-      const baseURL = 'http://localhost:3000/api/v1/auth/';
       return $http({
         method: 'POST',
         url: baseURL + 'register',
