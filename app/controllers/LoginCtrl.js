87ddd31bc47a3a6f76cad8f878af0e54c100672f
Jufebrown <jufebrown@gmail.com>
2017-05-11 15:41:03 -0500

    rewrote login function on controller

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -1,19 +1,20 @@
 `use strict`
 
-app.controller('LoginCtrl', function($rootScope, $scope, $location, $auth, $window) {
+app.controller('LoginCtrl', function($scope, $location, authFactory) {
 
-  $scope.emailLogin = function() {
-    $auth.login({ email: $scope.email, password: $scope.password })
+  $scope.login = function() {
+    authFactory.login($scope.user)
     .then(function(response) {
-      $window.localStorage.currentUser = JSON.stringify(response.data.user);
-      $rootScope.currentUser = JSON.parse($window.localStorage.currentUser);
+      localStorage.setItem('username', $scope.user.username)
+      localStorage.setItem('token', response.data.token)
+      console.log(localStorage)
     })
     .catch(function(response) {
       $scope.errorMessage = {};
       angular.forEach(response.data.message, function(message, field) {
         $scope.loginForm[field].$setValidity('server', false);
         $scope.errorMessage[field] = response.data.message[field];
-      });
-    });
-  };
-});
+      })
+    })
+  }
+})
