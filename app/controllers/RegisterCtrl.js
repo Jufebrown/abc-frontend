9b8c03ab84285dfb1abe907ae7e52b5f1605c7bf
Jufebrown <jufebrown@gmail.com>
2017-05-11 12:54:29 -0500

    removed toastr references

--- app/controllers/RegisterCtrl.js
+++ app/controllers/RegisterCtrl.js
@@ -1,15 +1,15 @@
 `use strict`
 
-app.controller('SignupCtrl', function($scope, $location, $auth, toastr) {
+app.controller('SignupCtrl', function($scope, $location, $auth) {
     $scope.signup = function() {
       $auth.signup($scope.user)
         .then(function(response) {
           $auth.setToken(response);
           $location.path('/');
-          toastr.info('You have successfully created a new account and have been signed-in');
+          console.log('You have successfully created a new account and have been signed-in');
         })
         .catch(function(response) {
-          toastr.error(response.data.message);
+          console.error(response.data.message);
         });
     };
   });
