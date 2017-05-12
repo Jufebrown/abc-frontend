ae115af5228cad4a35ca3784d16cacb974a58a59
Jufebrown <jufebrown@gmail.com>
2017-05-11 23:05:12 -0500

    added function to set login status

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -2,4 +2,27 @@
 
 app.controller('HomeCtrl', function($scope, $location, authFactory) {
 
-});
+  $scope.authStatus = function() {
+    $scope.isLoggedIn = false
+    const token = localStorage.token
+    if(token){
+      authFactory.ensureAuthenticated(token)
+      .then((user) => {
+        if (user.data.status === 'success')
+        $scope.isLoggedIn = true;
+        $scope.username = localStorage.username
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+
+  $scope.logout = () => {
+    authFactory.logout()
+    $location.path('/')
+  }
+
+  $scope.authStatus()
+
+})
