1189a371652d03534c12b4d4b892b818bbc23d63
Jufebrown <jufebrown@gmail.com>
2017-05-12 15:40:29 -0500

    fixed problems with cors

--- app/controllers/GameCtrl.js
+++ app/controllers/GameCtrl.js
@@ -2,19 +2,32 @@
 
 app.controller('GameCtrl', function($scope, $location, gameFactory) {
 
+  $scope.gameState = {
+                       question: true,
+                       thinking: false,
+                       correct: false,
+                       incorrect: false
+                     }
+
   $scope.username = localStorage.getItem('username')
 
   $scope.questionLetter = gameFactory.getRandomLetter()
 
-  // $scope.checkAnimal = function() {
-  //   gameFactory.checkABCWord($scope.answer)
-  //   .then(function(response) {
-  //     if(response === null) {
+  $scope.checkAnimal = function() {
+    $scope.gameState = {
+                         question: false,
+                         thinking: true,
+                         correct: false,
+                         incorrect: false
+                       }
+    gameFactory.checkABCWord($scope.answer)
+    // .then(function(response) {
+    //   if(response === null) {
 
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
-  // }
+    //   }
+    // })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 })
--- app/factories/gameFactory.js
+++ app/factories/gameFactory.js
@@ -1,11 +1,29 @@
 app.factory('gameFactory', function($http, $q) {
 
+  const token = localStorage.token
+
   return {
 
     getRandomLetter: () => String.fromCharCode(97 + Math.floor(Math.random() * 26)).toUpperCase(),
 
-
-
+    checkABCWord: (answer) => {
+      return $http({
+        method: 'GET',
+        url: `http://localhost:3000/api/v1/word/${answer}`,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token
+        }
+      })
+      .then((response) => {
+        if(response === null) {
+          console.log('response', response)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
 
   }
 })
