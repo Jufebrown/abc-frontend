125d4af83f2d369a82f3d18f5222b6a2e257db6e
Jufebrown <jufebrown@gmail.com>
2017-05-14 16:50:07 -0500

    added functions for checking answer

--- app/factories/gameFactory.js
+++ app/factories/gameFactory.js
@@ -13,7 +13,42 @@ app.factory('gameFactory', function($http, $q) {
       }
     },
 
-    checkABCWord: (answer) => {
+    analyzeSpeciesApiResults: (answer, data) => {
+      // checks to see if any results were returned
+      if (data.length > 0) {
+        for (var i = 0; i < data.length; i++) {
+          // checks to see if there are any vernacularNames
+          if (data[i].vernacularNames > 0) {
+            for (var j = 0; j < data[i].vernacularNames.length; j++) {
+              // checks to see if vernacularName is the same as the answer
+              if(data[i].vernacularNames[j].vernacularName === answer) {
+                // checks to see if there is a kingdom key
+                if (data[i].kingdom) {
+                  // checks to see if kingdom is correct to make the creature an animal
+                  if (data[i].kingdom === 'animalia' || 'metazoa') {
+                    return true
+                  } else {
+                    return false
+                  }
+                }
+              } else {
+                return false
+              }
+            }
+          }
+        }
+      } else {
+        return false
+      }
+    },
+
+    searchSpeciesApi: function(animal) {
+      let speciesUrl = `http://api.gbif.org/v1/species/search?q=` + animal
+      console.log('speciesUrl', speciesUrl)
+      return $http.get(speciesUrl)
+    },
+
+    checkAnswer: function(answer) {
       return $http({
         method: 'GET',
         url: `http://localhost:3000/api/v1/word/${answer}`,
