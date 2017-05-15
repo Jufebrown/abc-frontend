35577fbdb52a9c6b3dc97ccaa0d9434f486ebff9
Jufebrown <jufebrown@gmail.com>
2017-05-15 14:42:18 -0500

    refined logic for analyzing api data

--- app/factories/gameFactory.js
+++ app/factories/gameFactory.js
@@ -28,26 +28,32 @@ app.factory('gameFactory', function($http, $q) {
     },
 
     analyzeSpeciesApiResults: (answer, data) => {
+      console.log('data from analyze', data)
       // checks to see if any results were returned
       if (data.length > 0) {
         console.log('data is not 0 length')
         // loops through data objects
         for (var i = 0; i < data.length; i++) {
           // checks to see if there is a canonical name
-          if (data.canonicalName) {
-            console.log('there is a canonical name')
+          if (data[i].canonicalName) {
+            console.log('there is a canonical name', data[i].canonicalName.toLowerCase())
             // checks to see if canonical name is the same as the answer
-            if (data.canonicalName.toLowerCase() === answer.toLowerCase()) {
+            if (data[i].canonicalName.toLowerCase() === answer.toLowerCase()) {
               console.log('canonicalName is the same as answer')
               // checks to see if it's in the animal kingdom
-              if (data[i].kingdom.toLowerCase() === 'animalia' || 'metazoa') {
-                console.log('kindom is animal or metazoa, should return true')
-                return true
+              if (data[i].kingdom) {
+                // checks to see if kingdom is correct to make the creature an animal
+                if (data[i].kingdom.toLowerCase() === 'animalia' || 'metazoa') {
+                  return true
+                }
               }
             }
-          } else if (data[i].vernacularNames > 0) {
+          }
+          if (data[i].vernacularNames.length > 0) {
+            console.log('there is at least one vernacular name')
             // loops through vernacular names
             for (var j = 0; j < data[i].vernacularNames.length; j++) {
+              console.log(data[i].vernacularNames[j].vernacularName.toLowerCase())
               // checks to see if vernacularName is the same as the answer
               if(data[i].vernacularNames[j].vernacularName.toLowerCase() === answer.toLowerCase()) {
                 console.log('vernacular name is same as answer')
@@ -63,8 +69,9 @@ app.factory('gameFactory', function($http, $q) {
               }
             }
           }
-        }
-      } else {
+        } //end of for data.length
+      } else if(data.length === 0) {
+        console.log('data length = 0')
         // if no data, returns false
         return false
       }
