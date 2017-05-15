4b0a476dba3934e27a333143381368fc61d46c4d
Jufebrown <jufebrown@gmail.com>
2017-05-15 12:09:47 -0500

    changed url for api search

--- app/factories/gameFactory.js
+++ app/factories/gameFactory.js
@@ -73,7 +73,7 @@ app.factory('gameFactory', function($http, $q) {
     },
 
     searchSpeciesApi: function(animal) {
-      let speciesUrl = `http://api.gbif.org/v1/species/search?q=` + animal
+      let speciesUrl = `http://api.gbif.org/v1/species/search?q=${animal}&rank=GENUS`
       console.log('speciesUrl', speciesUrl)
       return $http.get(speciesUrl)
     },
