6b5c5f903bc814009d5a18ac6ac118ea89a17067
Jufebrown <jufebrown@gmail.com>
2017-05-15 12:08:12 -0500

    added new game method to homeCtrl

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -20,6 +20,17 @@ app.controller('HomeCtrl', function($scope, $location, authFactory, gameFactory)
     }
   }
 
+  $scope.newGame = () => {
+    localStorage.questionCount = 0
+    localStorage.correctAnswerCount = 0
+    localStorage.incorrectAnswerCount = 0
+    // gameFactory.newGame()
+    // .then((res) => {
+    //   console.log('res', res)
+    // })
+    $location.url('/game')
+  }
+
   $scope.logout = () => {
     authFactory.logout()
     $location.url('/')
